74838f4509b26fe0d2f5ab40c20852c05a0dc02c
Zafer <131403129+zaferErbil@users.noreply.github.com>
2023-10-24 15:49:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ summary=st.write("Welcome to the Wine Quality Evaluator! 🍷📊"
                  "Discover the true quality of your wine with our Python-based application. "
                  "Utilizing a Kaggle dataset, this app simplifies the process of assessing wine quality. "
                  "Just input your wine's key characteristics, and we'll provide an insightful analysis. "
-                 "While we can't predict the exact accuracy, we're here to make your wine experience more enjoyable and informed. Cheers! 🍇"
-,st.write("Access my application codes [here](https://github.com/zaferErbil/WineTaste).")
+                 "While we can't predict the exact accuracy, we're here to make your wine experience more enjoyable and informed. Cheers! 🍇")
+st.write("Access my application codes [here](https://github.com/zaferErbil/WineTaste).")
 
 
 fixaci=st.slider("Select Fixed Acid Value: ", min_value=4.6,max_value=15.9,value=5.0,step=0.1)
